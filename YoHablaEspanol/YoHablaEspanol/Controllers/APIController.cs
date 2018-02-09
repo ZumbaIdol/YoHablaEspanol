6024258a46576e4378a3ceff35fd0a831f130775
David A. Foote <david.a.foote@gmail.com>
2018-02-09 10:23:15 -0600

    Viewpage is displaying!

--- YoHablaEspanol/YoHablaEspanol/Controllers/APIController.cs
+++ YoHablaEspanol/YoHablaEspanol/Controllers/APIController.cs
@@ -9,37 +9,33 @@ namespace YoHablaEspanol.Controllers
 {
     public class APIController : Controller
     {
-        private IActionResult index;
-        public IActionResult Index { get => index; set => index = value; }
-    }
-
-    namespace OxfDictionary
-    {
-        class Program
+        public IActionResult Index()
         {
-            private static void Main()
+
+            HttpWebRequest req = null;
+
+            string PrimeUrl = "https://od-api.oxforddictionaries.com/api/v1";
+            string word = "test";
+            string endPoint = string.Format("/entries/en/{0}/translations=es", word);
+            req = (HttpWebRequest)HttpWebRequest.Create(string.Format("{0}{1}", PrimeUrl, endPoint));
+
+            //These are not network credentials, just custom headers
+            req.Headers.Add("app_id", "e6ecf63a");
+            req.Headers.Add("app_key", "9ac2d4671bbbee67be05202d3d0d3aae");
+
+            req.Method = WebRequestMethods.Http.Get;
+            req.Accept = "application/json";
+
+            using (HttpWebResponse HWR_Response = (HttpWebResponse)req.GetResponse())
+            using (Stream respStream = HWR_Response.GetResponseStream())
+            using (StreamReader sr = new StreamReader(respStream, Encoding.UTF8))
             {
-                HttpWebRequest req = null;
-
-                string PrimeUrl = "https://od-api.oxforddictionaries.com:443/api/v1/entries/es/";
-                req = (HttpWebRequest)HttpWebRequest.Create(PrimeUrl);
-
-                //These are not network credentials, just custom headers
-                req.Headers.Add("app_id", "e6ecf63a");
-                req.Headers.Add("app_key", "9ac2d4671bbbee67be05202d3d0d3aae");
-
-                req.Method = WebRequestMethods.Http.Get;
-                req.Accept = "application/json";
-
-                using (HttpWebResponse HWR_Response = (HttpWebResponse)req.GetResponse())
-                using (Stream respStream = HWR_Response.GetResponseStream())
-                using (StreamReader sr = new StreamReader(respStream, Encoding.UTF8))
-                {
-                    string theJson = sr.ReadToEnd();
-                    var jsonObject = JsonConvert.DeserializeObject(theJson);
-                }
+                string theJson = sr.ReadToEnd();
+                var jsonObject = JsonConvert.DeserializeObject(theJson);
             }
+
+            return View();
         }
-    }  
+    }
 }
     

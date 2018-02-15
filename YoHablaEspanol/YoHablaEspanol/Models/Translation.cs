3cd96634c797096052c758cc7dc00134bea41050
David A. Foote <david.a.foote@gmail.com>
2018-02-14 23:19:57 -0600

    Worked on a word list to display words to translate; saved in separate file.

--- YoHablaEspanol/YoHablaEspanol/Models/Translation.cs
+++ YoHablaEspanol/YoHablaEspanol/Models/Translation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace YoHablaEspanol.Models
+﻿namespace YoHablaEspanol.Models
 {
 
     public class Rootobject

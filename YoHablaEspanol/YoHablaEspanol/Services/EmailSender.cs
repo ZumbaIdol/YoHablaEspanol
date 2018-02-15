ae360dab928e0127d43f9c093fab18a98a2b7e7f
David A. Foote <david.a.foote@gmail.com>
2018-02-15 00:36:28 -0600

    Added code for SendGrid to be able to send emails but having issues...

--- YoHablaEspanol/YoHablaEspanol/Services/EmailSender.cs
+++ YoHablaEspanol/YoHablaEspanol/Services/EmailSender.cs
@@ -14,6 +14,31 @@ namespace YoHablaEspanol.Services
             return Task.CompletedTask;
         }
     }
+
+
+namespace Example
+    {
+        internal class Example
+        {
+            private static void Main()
+            {
+                Execute().Wait();
+            }
+
+            static async Task Execute()
+            {
+                var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("david.a.foote@gmail.com", "David A. Foote");
+                var subject = "Sending with SendGrid is Fun";
+                var to = new EmailAddress("david.a.foote@gmail.com", "David A. Foote");
+                var plainTextContent = "and easy to do anywhere, even with C#";
+                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+        }
+    }
 }
 
 

29eea663f296bb51d4f52822af0818535e1eb5cd
David A. Foote <david.a.foote@gmail.com>
2018-02-19 13:17:53 -0600

    Trying to get Twilio and SendGrid working to no avail...

--- YoHablaEspanol/YoHablaEspanol/Services/EmailSender.cs
+++ YoHablaEspanol/YoHablaEspanol/Services/EmailSender.cs
@@ -1,7 +1,4 @@
-﻿using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace YoHablaEspanol.Services
 {
@@ -16,29 +13,7 @@ namespace YoHablaEspanol.Services
     }
 
 
-namespace Example
-    {
-        internal class Example
-        {
-            private static void Main()
-            {
-                Execute().Wait();
-            }
 
-            static async Task Execute()
-            {
-                var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
-                var client = new SendGridClient(apiKey);
-                var from = new EmailAddress("david.a.foote@gmail.com", "David A. Foote");
-                var subject = "Sending with SendGrid is Fun";
-                var to = new EmailAddress("david.a.foote@gmail.com", "David A. Foote");
-                var plainTextContent = "and easy to do anywhere, even with C#";
-                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-                var response = await client.SendEmailAsync(msg);
-            }
-        }
-    }
 }
 
 

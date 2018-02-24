41b187845a18512426a16438db866d1c0dd40b42
David A. Foote <david.a.foote@gmail.com>
2018-02-24 08:34:51 -0600

    Working on randomly displaying dictionary words, NotImplementationException...

--- YoHablaEspanol/YoHablaEspanol/Controllers/APIController.cs
+++ YoHablaEspanol/YoHablaEspanol/Controllers/APIController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using YoHablaEspanol.Models;
@@ -15,11 +17,36 @@ namespace YoHablaEspanol.Controllers
         {
             //Pick a word from the database
 
+            IDictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add(new KeyValuePair<string, string>("feliz", "happy"));
+            dict.Add(new KeyValuePair<string, string>("cambiar", "change"));
+            dict.Add(new KeyValuePair<string, string>("corre", "run"));
+            dict.Add(new KeyValuePair<string, string>("inmediatamente", "immediately"));
+            dict.Add(new KeyValuePair<string, string>("rojo", "red"));
+
             //Put that on the page somewhere
+            IEnumerable<TValue> RandomValues<TKey, TValue>(IDictionary<TKey, TValue> dictionary)
+            {
+                Random rand = new Random();
+                List<TValue> values = Enumerable.ToList(dictionary.Values);
+                int size = dictionary.Count;
+                while (true)
+                {
+                    yield return values[rand.Next(size)];
+                }
+            }
+
+            Dictionary<string, object> getDict = GetDictionary();
+            foreach (object value in RandomValues(dict).Take(5))
+            {
+                return View();
+            }
 
             //New view model here
             return View();
         }
+
+        private Dictionary<string, object> GetDictionary() => throw new NotImplementedException();
 
         [HttpPost]
         public IActionResult Index(string translation, string pretranslation)
